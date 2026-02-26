b3effbc534ada8a114e5496bf5b225cc988ef73a
kerikir <vlasov-kerik@mail.ru>
2026-02-26 17:26:58 +0300

    added a data source for parameterized tests

---
+++ src/test/kotlin/arraylist/NumbersMutableListTest.kt
@@ -0,0 +1,11 @@
+package arraylist
+
+import org.junit.jupiter.api.Assertions.*
+
+class NumbersMutableListTest {
+
+    companion object {
+        @JvmStatic
+        private fun numbersMutableListSource() = listOf(NumbersArrayList())
+    }
+}
