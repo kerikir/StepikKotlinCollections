ebddff81f07fd568292fc54b78993e405a530913
kerikir <vlasov-kerik@mail.ru>
2026-02-26 16:26:58 +0300

    added the class NumbersArrayList

---
+++ src/main/kotlin/arraylist/NumbersArrayList.kt
@@ -0,0 +1,43 @@
+package arraylist
+
+class NumbersArrayList : NumbersMutableList {
+
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun get(index: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(index: Int, number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeAt(index: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(number: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun plus(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun minus(number: Int) {
+        TODO("Not yet implemented")
+    }
+}
