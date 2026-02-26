05fb85e3e8b161c016c825c1e8685c28572cf35b
kerikir <vlasov-kerik@mail.ru>
2026-02-26 22:34:12 +0300

    added test When element removed then size decreased

--- src/test/kotlin/arraylist/NumbersMutableListTest.kt
+++ src/test/kotlin/arraylist/NumbersMutableListTest.kt
@@ -1,8 +1,20 @@
 package arraylist
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
 
 class NumbersMutableListTest {
+
+    @ParameterizedTest
+    @MethodSource("numbersMutableListSource")
+    fun `When element removed then size decreased`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        list.remove(50)
+        assertEquals(99, list.size)
+    }
 
     companion object {
         @JvmStatic
