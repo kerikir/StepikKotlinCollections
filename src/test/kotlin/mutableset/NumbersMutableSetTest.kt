40fac21a2bcbf498dce2efdf89526820480598eb
kerikir <vlasov-kerik@mail.ru>
2026-03-01 00:49:33 +0300

    added test When added 100 elements Then size 100

--- src/test/kotlin/mutableset/NumbersMutableSetTest.kt
+++ src/test/kotlin/mutableset/NumbersMutableSetTest.kt
@@ -1,8 +1,18 @@
 package mutableset
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class NumbersMutableSetTest {
 
     private val numbers = NumbersHashSet()
+
+
+    @Test
+    fun `When added 100 elements Then size 100`() {
+		repeat(100) {
+            numbers.add(it)
+        }
+        assertEquals(100, numbers.size)
+    }
 }
