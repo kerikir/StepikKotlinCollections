3d525e3cf64fd9578dd9c24a4f9f2873afd4e82b
kerikir <vlasov-kerik@mail.ru>
2026-03-01 00:40:55 +0300

    added the class HashSet

---
+++ src/main/kotlin/mutableset/NumbersHashSet.kt
@@ -0,0 +1,23 @@
+package mutableset
+
+class NumbersHashSet : NumbersMutableSet {
+
+    override var size: Int = 0
+        private set
+
+    override fun add(number: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(number: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+}
