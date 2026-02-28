864ed5f9bb0db7ac9cf0100dc66cd84a4f669598
kerikir <vlasov-kerik@mail.ru>
2026-02-28 22:28:35 +0300

    added the class NumbersDoublyLinkedList

---
+++ src/main/kotlin/mutablelist/NumbersDoublyLinkedList.kt
@@ -0,0 +1,43 @@
+package mutablelist
+
+class NumbersDoublyLinkedList : NumbersMutableList {
+
+    override var size: Int = 0
+        private set
+
+    override fun get(index: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(index: Int, number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeAt(index: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(number: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun plus(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun minus(number: Int) {
+        TODO("Not yet implemented")
+    }
+}
