73827ab3e4ae279d1628b6ba40c81ff924d09e45
kerikir <vlasov-kerik@mail.ru>
2026-02-28 21:36:11 +0300

    added test When method add invoked with negative index then exception is thrown

--- src/test/kotlin/mutablelist/NumbersMutableListTest.kt
+++ src/test/kotlin/mutablelist/NumbersMutableListTest.kt
@@ -158,6 +158,17 @@ class NumbersMutableListTest {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("numbersMutableListSource")
+    fun `When method add invoked with negative index then exception is thrown`(list: NumbersMutableList) {
+        repeat(10) {
+            list.add(it)
+        }
+        assertThrows<IndexOutOfBoundsException> {
+            list.add(-1, 100)
+        }
+    }
+
     companion object {
         @JvmStatic
         private fun numbersMutableListSource() = listOf(NumbersArrayList(), NumbersSinglyLinkedList())
