1c2505be5b634a666b7310ae1a7913eb4d19033a
kerikir <vlasov-kerik@mail.ru>
2026-03-02 22:11:15 +0300

    added the generic class ArrayList

---
+++ src/main/kotlin/generic/MyArrayList.kt
@@ -0,0 +1,39 @@
+package generic
+
+class MyArrayList<T> : MyMutableList<T> {
+
+    override fun iterator(): Iterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun add(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(index: Int, element: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(element: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(index: Int): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeAt(index: Int) {
+        TODO("Not yet implemented")
+    }
+}
