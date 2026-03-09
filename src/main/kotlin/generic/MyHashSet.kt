0c64e37362f7fc267885ada170106acfa297c609
kerikir <vlasov-kerik@mail.ru>
2026-03-09 22:02:23 +0300

    added the class HashSet

---
+++ src/main/kotlin/generic/MyHashSet.kt
@@ -0,0 +1,27 @@
+package generic
+
+class MyHashSet<T> : MyMutableSet<T> {
+    
+    override fun iterator(): Iterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun add(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(element: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+}
