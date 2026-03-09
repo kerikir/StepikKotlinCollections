7c28f18103e97b515339235a6d0f67647fd2f22d
kerikir <vlasov-kerik@mail.ru>
2026-03-09 22:05:05 +0300

    added the companion object

--- src/main/kotlin/generic/MyHashSet.kt
+++ src/main/kotlin/generic/MyHashSet.kt
@@ -1,6 +1,13 @@
 package generic
 
+import mutableset.NumbersHashSet
+import mutableset.NumbersHashSet.Companion
+import mutableset.NumbersHashSet.Node
+
 class MyHashSet<T> : MyMutableSet<T> {
+
+    private var elements = arrayOfNulls<Any>(INITIAL_CAPACITY)
+
     
     override fun iterator(): Iterator<T> {
         TODO("Not yet implemented")
@@ -23,5 +30,11 @@ class MyHashSet<T> : MyMutableSet<T> {
 
     override fun clear() {
         TODO("Not yet implemented")
+    }
+
+
+    companion object {
+        private const val INITIAL_CAPACITY = 16
+        private const val LOAD_FACTORY = 0.75
     }
 }
