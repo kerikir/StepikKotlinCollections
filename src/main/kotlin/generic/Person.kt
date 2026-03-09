0121541069b8083415bc94baac64a92a88cd0be7
kerikir <vlasov-kerik@mail.ru>
2026-03-09 23:37:24 +0300

    redefined the equals method for the Person class

--- src/main/kotlin/generic/Person.kt
+++ src/main/kotlin/generic/Person.kt
@@ -8,4 +8,13 @@ class Person(
     override fun toString(): String {
         return "${this.name}, ${this.age}"
     }
+
+
+    override fun equals(other: Any?): Boolean {
+
+        if (this === other) return true
+        if (other !is Person) return false
+
+        return this.name == other.name && this.age == other.age
+    }
 }
