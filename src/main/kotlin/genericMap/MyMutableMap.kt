f6c1c6363453acd5a570dadd465ae587cb5591a7
kerikir <vlasov-kerik@mail.ru>
2026-03-10 19:19:59 +0300

    added the MutableMap interface

---
+++ src/main/kotlin/genericMap/MyMutableMap.kt
@@ -0,0 +1,25 @@
+package genericMap
+
+import generic.MyMutableCollection
+import generic.MyMutableSet
+
+interface MyMutableMap<K, V> {
+
+    val size: Int
+
+    fun put(key: K, value: V): V?
+
+    operator fun get(key: K): V?
+
+    fun containsKey(key: K): Boolean
+
+    fun containsValue(value: V): Boolean
+
+    fun clear()
+
+    fun remove(key: K): V?
+
+    val keys: MyMutableSet<K>
+
+    val values: MyMutableCollection<V>
+}
